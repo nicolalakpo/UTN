cccc49c349fd7fb9ed9c3ebb8f36e4bd7dab3062
Diego_Lehmann <nicolasdlehmann@hotmail.com>
2022-07-01 20:57:26 -0300

    inicios de react

---
+++ ProgramadorFullStack/Unidad 4/react/frontt/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Nav from "./components/layout/Nav";
+import Footer from "./components/layout/Footer"
+import Header from "./components/layout/Header"
+
+function App() {
+  return (
+    <div className="App">
+        <Footer/>
+        <Nav/>
+        <Header/>
+        
+    </div>
+  );
+}
+
+export default App;
---
+++ ProgramadorFullStack/Unidad 4/react/frontt/src/components/layout/Nav.js
@@ -0,0 +1,7 @@
+import React from "react"
+const Footer = (props) =>{
+    return(
+        <div>Este es el Nav</div>
+    )
+}
+export default Footer;
