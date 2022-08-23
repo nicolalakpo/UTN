86083a4160b06110c52bf7dd1cab55f2b027cb66
Diego_Lehmann <nicolasdlehmann@hotmail.com>
2022-08-23 17:57:09 -0300

    implementa nuevamente lo de pablo

---
+++ ProgramadorFullStack/ZProyecto Final/react/frontt/src/export/imges.js
@@ -0,0 +1,8 @@
+import bowni from '../images/bowni.jpeg';
+import brownitop from '../images/browni top.jpeg';
+import lemon from '../images/lemon.jpeg';
+import lemontop from '../images/lemon top.jpeg';
+const lemontop2= require('../images/lemon top.jpeg')
+
+
+export default [bowni, brownitop, lemon, lemontop2];
