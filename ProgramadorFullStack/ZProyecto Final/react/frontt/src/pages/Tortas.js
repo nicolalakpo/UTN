e34dd4407221a16d1752118ae53a4b59130d68ed
Diego_Lehmann <nicolasdlehmann@hotmail.com>
2022-08-28 19:16:50 -0300

    ultimos arreglos

--- ProgramadorFullStack/ZProyecto Final/react/frontt/src/pages/Tortas.js
+++ ProgramadorFullStack/ZProyecto Final/react/frontt/src/pages/Tortas.js
@@ -6,7 +6,7 @@ import { motion } from "framer-motion";
 const Tortas = (props) => {
     return (
         <motion.div className="slider-container">
-            <motion.div className="slider" drag='x' dragConstraints={{ right: 0, left: -3000 }}>
+            <motion.div className="slider" drag='x' dragConstraints={{ right: 0, left: -2800 }}>
                 {images.map(image => (
                     <motion.div className="item">
                         <img src={image} alt="" />
