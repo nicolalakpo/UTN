261a08daccb7609a52fc200c72cb258beab0ee1c
Diego_Lehmann <nicolasdlehmann@hotmail.com>
2022-08-28 19:04:42 -0300

    listo para entregar

--- ProgramadorFullStack/ZProyecto Final/react/frontt/src/pages/EventosEspeciales.js
+++ ProgramadorFullStack/ZProyecto Final/react/frontt/src/pages/EventosEspeciales.js
@@ -76,6 +76,49 @@ const EventosEspeciales = (props) => {
             </div>
 
 
+            <div className="contenedor">
+                <div className="info">
+
+                    <h2>
+
+                        Torta de casamiento
+
+                    </h2>
+
+                    <p>
+                        <br />
+                        Biscochuelo de vainilla
+                        <br />
+                        cubierto de un fino glace
+                        <br />
+                        Relleno de dulce de leche
+
+                    </p>
+                </div>
+                <img src="IMG/TortaOficialCasamiento.jpg" alt="" />
+            </div>
+
+            <div className="contenedor">
+                <div className="info">
+
+                    <h2>
+
+                        Pastelitos
+
+                    </h2>
+
+                    <p>
+                        <br />
+                        cubierto de ojaldre
+                        <br />
+                        Relleno de batata o dulce de membrillo
+
+                    </p>
+                </div>
+                <img src="IMG/pastelitos.jpg" alt="" />
+            </div>
+
+
 
         </>
     )
